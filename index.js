d89ac1b65b9985a1f21a0eec98515a63e94e4bf9
ZOUNGRANA ALIMATA <142305844+ZoungranaAlimata@users.noreply.github.com>
2025-04-02 09:16:54 +0600

    connect to cluster

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const mongoose = require("mongoose")
 const express = require('express')
 const app = express()
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -8,4 +11,14 @@ app.get('/', function (req, res) {
 app.listen(4000, ()=> {
 
     console.log("Server is run on port 4000")
+});
+
+mongoose.connect("mongodb+srv://zoungranaa766:ZURq7uwTIfxYWrmG@cluster-1.w43yfj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1"
+
+).then(() => {
+  console.log("Connected to MongoDB");
+
+}).catch((err) => {
+  console.log("Failed to connect to Mongo", err);
+
 });
